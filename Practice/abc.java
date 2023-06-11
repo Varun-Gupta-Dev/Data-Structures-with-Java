15cb053a1f42baa276616ad1dee1f040f3a204b9
Varun Gupta <varunguptaalg@gmail.com>
2023-06-12 01:33:48 +0530

    my commit

--- Practice/abc.java
+++ Practice/abc.java
@@ -3,7 +3,7 @@ package Practice;
 public class abc {
    public static void main(String args[]){
         char arr[] = {'a','b','c'};
-        arr.remove();
+        
         System.out.println(arr);
    } 
 }
